e1123d07e605a07e1b8756da7c2663f1553f9b36
Etido Ukpong <etido.ukpong@madetech.com>
2026-01-26 18:03:48 +0000

    test html file displays duties passes

--- test_themes.py
+++ test_themes.py
@@ -1,12 +1,8 @@
 from themes import list_of_duties, Duty
-from requests_html import HTML
-import re
 
 """
 Finished product: write a program that would display on the screen all the duties in this apprenticeship. 
 """
-
-# TODO: Use RE groups and Match to check that the number group for each duty contans a unique number.
 
 def test_correct_number_of_duties_are_listed():
     number_of_duties = 13
@@ -29,8 +25,8 @@ def test_html_file_is_created():
         assert False
 
 def test_html_file_displays_duties():
-    with open("duties.html") as page:
-        source = page.read()
-        html = HTML(html=source)
+    page = open("duties.html", "r")
+    html = page.read()
+    print(type(html))
     for duty in list_of_duties:
-        assert duty.text in html.text
+        assert duty.text in html
